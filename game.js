96bfa383480529b902abaf6a3723c9d2646d61ca
Dmitrii <dmitry.valiev174@gmail.com>
2022-03-07 23:44:39 +0500

    Make Game func

--- game.js
+++ game.js
@@ -1,30 +1,79 @@
 'use strict'
 
+const scope = ['rock', 'scissors', 'paper']
+
 function capitalize(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 function computerPlay() {
-    const scope = ['rock', 'scissors', 'paper'];
     const index = Math.floor(Math.random() * scope.length);
     return scope[index];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
     if (playerSelection === computerSelection) {
-        return `Draw! Both players chose ${capitalize(playerSelection)}`;
+        return {
+            'winner': 'draw',
+            'playerSelection': capitalize(playerSelection),
+            'computerSelection': capitalize(computerSelection)
+        };
     } else if (playerSelection === 'rock' && computerSelection === 'scissors'
         || playerSelection === 'paper' && computerSelection === 'rock'
         || playerSelection === 'scissors' && computerSelection === 'paper') {
-        return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+        return {
+            'winner': "player",
+            'playerSelection': capitalize(playerSelection),
+            'computerSelection': capitalize(computerSelection)
+        };
     } else {
-        return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
+        return {
+            'winner': "computer",
+            'playerSelection': capitalize(playerSelection),
+            'computerSelection': capitalize(computerSelection)
+        };
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function showCurrentScores(playerScores, computerScores) {
+    console.log(`Player: ${playerScores}, Computer: ${computerScores}`);
+}
+
+function game() {
+    let computerScores = 0;
+    let playerScores = 0;
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay();
+        let playerSelection = prompt('Please choose: Rock, Scissors or Paper')
+            .toLowerCase();
+
+        const result = playRound(playerSelection, computerSelection);
+
+        switch (result.winner) {
+            case 'draw':
+                console.log(`Draw! Both of players chose ${result.computerSelection}`);
+                showCurrentScores(playerScores, computerScores);
+                break;
+            case "player":
+                console.log(`You Win! ${result.playerSelection} beats ${result.computerSelection}`);
+                playerScores++;
+                showCurrentScores(playerScores, computerScores);
+                break;
+            case 'computer':
+                console.log(`You Lose! ${result.computerSelection} beats ${result.playerSelection}`);
+                computerScores++;
+                showCurrentScores(playerScores, computerScores);
+                break;
+        }
+    }
+
+    if (playerScores > computerScores) {
+        console.log(`You Win! ${playerScores}:${computerScores}`);
+    } else if (playerScores < computerScores) {
+        console.log(`You Lose! ${playerScores}:${computerScores}`);
+    } else {
+        console.log(`Draw! ${playerScores}:${computerScores}`);
+    }
+}
+
+game();
